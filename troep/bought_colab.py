a317d88183ecab0fb8c736b69f6329df6945527d
KaiterHorst <60784517+KaiterHorst@users.noreply.github.com>
2020-04-08 23:52:38 +0200

    Update Bought_Colab
    de input moet nog over alles producten kunnen itereren, niet maar alleen 9397, het bijbehorende profiel moet ook nog in de list van similair profiles komen, de eerste dan weetje met welke ze te vergelijken zijn.

--- troep/bought_colab.py
+++ troep/bought_colab.py
@@ -4,20 +4,31 @@ with open('sessions_data_bought_items.csv', newline='') as f:
     reader = csv.reader(f)
     data = []
     winkelwagens = []
+    profielen = []
     for row in reader:
         data.append(list(row))
 
 for x in data:
-    if len(x) > 6:
+    if len(x) > 6:#6 omdat je de profid en het zelfde product er af haalt, dan blijven er 4 over voor de frontend.
         winkelwagens.append(x[1:])
+        profielen.append(x[0])
 
-input = str(9397)
+
+input = str(9397) #product id als input ( moet nog over alle producten itereren)
+
 similairwinkelwagen = []
+similairprofiles = []
 for winkelwagen in winkelwagens:
     for product in winkelwagen:
         if input == product:
             similairwinkelwagen = winkelwagen
-            similairwinkelwagen.remove(product)
+            positie = winkelwagens.index(winkelwagen)
 
-print(similairwinkelwagen)
+            while len(similairprofiles) < 4:
+                similairprofiles.append(profielen[positie])
+
+with open('vergelijkbare_profielen.csv', 'w', newline='') as csvfile: #schrijft de vergelijkbare profielen naar een file
+        writer = csv.writer(csvfile, delimiter=' ',
+        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(similairprofiles)
 
