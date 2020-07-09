82b115e03c503850ebd2e31d5827ec9e12022916
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2020-07-09 18:02:16 +0200

    Update fill_database.py

--- complete_project/2. Fill Database/fill_database.py
+++ complete_project/2. Fill Database/fill_database.py
@@ -3,8 +3,8 @@ import random
 import csv
 from collections import Counter
 
-```This script is ran after creating the database tables, this script fill the tables with the required information,
-the tables will contain the data needed to run the recommendation engines.```
+'''This script is ran after creating the database tables, this script fill the tables with the required information,
+the tables will contain the data needed to run the recommendation engines.'''
 
 
 
@@ -12,8 +12,8 @@ connection = psycopg2.connect("dbname=Huwebshop user=postgres password=1qaz2wsx3
 cur = connection.cursor()
 
 def insert_similar_items_content_filter(list_similar_items_id):
-	```This function fill the table similar_products with the data form list_similar_items_id, 
-    this table will contain the data of which products are similar of eachother.```
+'''This function fill the table similar_products with the data form list_similar_items_id, 
+    this table will contain the data of which products are similar of eachother.'''
 
     cur.execute("DELETE FROM similar_products;")
 
@@ -25,9 +25,9 @@ def insert_similar_items_content_filter(list_similar_items_id):
     connection.commit()
 
 def get_all_items():
-	```This function gets all the data needed for the function insert_similar_items_content_filter, 
-	it produces the list list_similar_items_id which is then given to the next function insert_similar_items_content_filter, 
-    the similarity is determined by looking which products have the same brand and subsubcategory```
+'''This function gets all the data needed for the function insert_similar_items_content_filter, 
+it produces the list list_similar_items_id which is then given to the next function insert_similar_items_content_filter, 
+the similarity is determined by looking which products have the same brand and subsubcategory'''
 
     cur.execute("""SELECT id, brand, subsubcategory FROM products WHERE brand IS NOT NULL;""")
     myresult = cur.fetchall()
@@ -47,14 +47,14 @@ def get_all_items():
 
 
 def insert_items_table_csv():
-	```This function fill the tables: products, profiles, profiles_previously_viewed, sessions, 
+	'''This function fill the tables: products, profiles, profiles_previously_viewed, sessions, 
     sessions_data_bought_items_single, and sessions_data_bought_items with the required data for
-    the recoommendation engines to use. All the data is obtained via the csv files containing all the information.```
+    the recoommendation engines to use. All the data is obtained via the csv files containing all the information.'''
     
     filenames = ['products', 'profiles', 'profiles_previously_viewed', 'sessions']
     path = 'sessions_data_bought_items.csv'
 
-	```Fills the table sessions_data_bought_items with the data of which items are bought togehter in a single session```
+	'''Fills the table sessions_data_bought_items with the data of which items are bought togehter in a single session'''
     cur.execute("DELETE FROM sessions_data_bought_items;")
     with open(path) as csvfile:
         print("Copying {}...".format(path))
@@ -69,14 +69,14 @@ def insert_items_table_csv():
             except:
                 continue
 
-	```Fills the tables products, profiles, profiles_previously_viewed, and sessions in order.```
+	'''Fills the tables products, profiles, profiles_previously_viewed, and sessions in order.'''
     for filename in filenames:
         with open(filename + '.csv') as csvfile:
             print("Copying {}...".format(filename))
             cur.copy_expert("COPY " + filename + " FROM STDIN DELIMITER ',' CSV HEADER", csvfile)
             connection.commit()
 
-	```Fills the table sessions_data_bought_items_single```
+	'''Fills the table sessions_data_bought_items_single'''
     with open('sessions_prodid.csv') as csvfile:
         print("Copying {}...".format('sessions_prodid.csv'))
         cur.copy_expert("COPY sessions_data_bought_items_single FROM STDIN DELIMITER ',' CSV HEADER", csvfile)
@@ -87,10 +87,10 @@ def insert_items_table_csv():
 
 
 def popular_products_fill():
-	```This function fill the table: popular_products with the required data for
+	'''This function fill the table: popular_products with the required data for
     the recoommendation engines to use. Each set of popular products is distinguished by
     the category of the items. Popular products is determined by seeing which item is the most often bought
-    in each category.```
+    in each category.'''
     
     query = "SELECT products FROM sessions_data_bought_items_single;"
     cur.execute(query)
