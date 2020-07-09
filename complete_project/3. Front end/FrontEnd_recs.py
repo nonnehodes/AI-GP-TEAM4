b2dc825b2012bbc90d9683d36e42be73e626071c
AdamMC-GL <60597883+AdamMC-GL@users.noreply.github.com>
2020-07-09 17:58:23 +0200

    Update FrontEnd_recs.py

--- complete_project/3. Front end/FrontEnd_recs.py
+++ complete_project/3. Front end/FrontEnd_recs.py
@@ -1,16 +1,16 @@
 import psycopg2
 from collections import Counter
 
-```This script is used by the front end, each function obtains the data from the filled database requested
+'''This script is used by the front end, each function obtains the data from the filled database requested
 by the front end to show the correct recommended products of their page. Each page uses a different function, 
-but if one funtion does not return enough items, other function are used to obtain data from.```
+but if one funtion does not return enough items, other function are used to obtain data from.'''
 
 connection = psycopg2.connect("dbname=Huwebshop user=postgres password=1qaz2wsx3edc")
 cur = connection.cursor()
 
 def product_page_rec(id):
-	```This function is used on the page of a product, it uses the table similar_products
-    to get products similar to the product of the page```
+	'''This function is used on the page of a product, it uses the table similar_products
+    to get products similar to the product of the page'''
     
     print(id)
     id = "\'" + id + "\'"
@@ -23,8 +23,8 @@ def product_page_rec(id):
 
 
 def shopping_cart_rec(id):
-	```This function is used on the page of your shopping cart, it uses the table sessions_data_bought_items2
-    to get products often bought together with a product in your shopping cart```
+	'''This function is used on the page of your shopping cart, it uses the table sessions_data_bought_items2
+    to get products often bought together with a product in your shopping cart'''
     
     all_results = []
     final_results = []
@@ -50,8 +50,8 @@ def shopping_cart_rec(id):
     return final_results
 
 def pop_prods_rec(cat):
-	```This function is used on the front page of each category page, it uses the table popular_products
-    to get products most often bought in each category```
+	'''This function is used on the front page of each category page, it uses the table popular_products
+    to get products most often bought in each category'''
     
     all_categories = ['Baby & kind', 'Wonen & vrije tijd', 'Huishouden', 'Gezond & verzorging', 'Eten & drinken',
                       'Make-up & geuren', 'Elektronica & media', 'Kleding & sieraden']
